631f2e841c66c551be1d12fd102e8c05ad87832e
viola <violachandra2806@gmail.com>
2026-01-04 22:54:51 +0700

    Viola update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "BASE_URL", "\"http://192.168.100.12:8080/chattingapp/\"")
+        buildConfigField("String", "BASE_URL", "\"http://192.168.18.97:8080/chattingapp/\"")
         buildConfigField("String", "SUPABASE_URL", "\"https://nxagvhkldfxenczfahch.supabase.co\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54YWd2aGtsZGZ4ZW5jemZhaGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0ODc4NTUsImV4cCI6MjA2MzA2Mzg1NX0.6fxDR2kDAzIbey_anvHyOM6FKhbcvOfPN0LMsKp2BuE\"")
     }
